2d0931e0dad7f379032a900b9fe38ab21795e8dc
Leonard Ladva <leonard.ladva@gmail.com>
2026-01-15 18:01:31 +0200

    removed unused imports

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -1,16 +1,10 @@
 package com.example.newsapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.ui.Modifier
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -48,9 +42,9 @@ class MainActivity : ComponentActivity() {
                             type = NavType.StringType
                         })
                     ) { backStackEntry ->
+                        // this encoding solution was made with AI
                         val encodedUrl = backStackEntry.arguments?.getString("articleUrl") ?: ""
                         val articleUrl = java.net.URLDecoder.decode(encodedUrl, "UTF-8")
-                        Log.d("MainActivity", "Article URL: $articleUrl")
                         ArticleScreen(
                             articleUrl = articleUrl,
                             navController = navController,
